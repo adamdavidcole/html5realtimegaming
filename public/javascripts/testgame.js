890eb5e3d1d7e69738722746ff7b5b79170d896b
Adam Cole <adamcole@roberts-MacBook-Pro-2.local>
2016-03-30 18:48:52 -0400

    cleaned up code

--- public/javascripts/testgame.js
+++ public/javascripts/testgame.js
@@ -2,7 +2,6 @@
  * Created by adamcole on 2/16/16.
  */
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-var graphics;
 var BACKGROUND_COLOR = '#ddd';
 var playersCollisionGroup;
 var arenaCollisionGroup;
@@ -16,9 +15,9 @@ var timer;
 
 var players;
 var player1;
-var player2;
 
 var moveVelocity = 100;
+var rotateVelocity = 200;
 
 function preload() {
     game.load.image('player', 'images/player.png');
@@ -38,77 +37,60 @@ function create() {
 
     //  Turn on impact events for the world, without this we get no collision callbacks
     game.physics.p2.setImpactEvents(true);
-    //  This part is vital if you want the objects with their own collision groups to still collide with the world bounds
-    //  (which we do) - what this does is adjust the bounds to use its own collision group.
+    // adjust the bounds to use its own collision group
     game.physics.p2.updateBoundsCollisionGroup();
 
+    // create collision groups
     playersCollisionGroup = game.physics.p2.createCollisionGroup();
     arenaCollisionGroup = game.physics.p2.createCollisionGroup();
     puckCollisionGroup = game.physics.p2.createCollisionGroup();
 
-    var arenaradius = 580/2;
+    // create arena
     arena = game.add.sprite(game.width / 2, game.height / 2, 'arena');
-
     game.physics.p2.enable(arena, true);
     arena.body.clearShapes();
     arena.body.loadPolygon('arenaStrokeData', 'arenaStroke');
     arena.body.setCollisionGroup(arenaCollisionGroup);
+    arena.body.static = true;
+    arena.body.collides(puckCollisionGroup);
 
-    var puckRadius = 5;
+    // create game puck
     puck = game.add.sprite(game.width / 2, game.height / 2, 'puck');
     puck.scale.setTo(.2,.2);
     game.physics.p2.enable(puck, true);
     puck.body.setCircle(puck.width / 2);
-
     puck.body.setCollisionGroup(puckCollisionGroup);
     puck.body.collides([playersCollisionGroup, arenaCollisionGroup]);
 
-    //arena.body.setCircle(arenaradius);
-    arena.body.static = true;
-    //arena.body.data.shapes[0].sensor = true;
-    arena.body.collides(puckCollisionGroup);
-
+    // create players group
     players = game.add.group();
     players.enableBody = true;
     players.physicsBodyType = Phaser.Physics.P2JS;
+
     player1 = createPlayer();
-    //player2 = createPlayer();
-    //arena.body.onEndContact.add(contactEnded, arena);
     cursors = game.input.keyboard.createCursorKeys();
     cursors.rotateRight = game.input.keyboard.addKey(Phaser.Keyboard.D);
     cursors.rotateLeft = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    cursors.spacebar =  game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    wasd = {
-        up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-        down: game.input.keyboard.addKey(Phaser.Keyboard.S),
-        left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-        right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-    };
-    spacebar =  game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    initiateTimer();
+};
 
-    //  Create our Timer
-    timer = game.time.create(false);
-
-    //  Set a TimerEvent to occur after 2 seconds
-    timer.loop(50, updateArenaSize, this);
-
-    //  Start the timer running - this is important!
-    //  It won't start automatically, allowing you to hook it to button events and the like.
-    timer.start();
-}
-
+// update the game state
 function update() {
-    updatePlayer(player1, cursors);
-    //updatePlayer(player2, wasd);
-    if (!isPlayerWithinArena(player1)) player1.kill();
-    //if (!isPlayerWithinArena(player2)) player2.kill();
-}
+    players.children.forEach(function (player) {
+        // update the player and check if it is in bounds
+        updatePlayer(player, cursors);
+        if (!isPlayerWithinArena(player)) player.kill();
+    });
+};
 
 var updatePlayer = function(player, controls) {
+    // controls to rotate character
     if (controls.rotateLeft.isDown) {
-        player.body.rotateLeft(200);
+        player.body.rotateLeft(rotateVelocity);
     } else if (controls.rotateRight.isDown) {
-        player.body.rotateRight(200);
+        player.body.rotateRight(rotateVelocity);
     } else {
         player.body.setZeroRotation();
     }
@@ -119,6 +101,7 @@ var updatePlayer = function(player, controls) {
     //    player.body.reverse(150);
     //}
 
+    // controls to move character along axes
     if (controls.left.isDown)
     {
         player.body.moveLeft(moveVelocity);
@@ -146,13 +129,12 @@ var createPlayer = function() {
     player.body.setCollisionGroup(playersCollisionGroup);
     player.body.collides([playersCollisionGroup, puckCollisionGroup]);
     player.body.debug = true;
-    player.body.onBeginContact.add(function(bodyA, bodyB, shapeA, shapeB, contactEquations) {
+    player.body.onBeginContact.add(function(bodyA, bodyB, shapeA, shapeB) {
         if (shapeA.constructor.name === "Circle") {
             player.body.debug = false;
             player.kill();
         }
     });
-    //player.body.onEndContact.add(contactEnded, player);
     player.body.mass = 2;
     player.body.damping = .8;
     //player.body.angularDamping = 0;
@@ -160,35 +142,16 @@ var createPlayer = function() {
     return player;
 };
 
-var contactEnded = function(bodyThisInContact, bodyThisEndedContact, shapeThatCausedContact, shapeFromContactBody) {
-    console.log(bodyThisInContact);
-    console.log(bodyThisEndedContact);
-    console.log(shapeThatCausedContact);
-    console.log(shapeFromContactBody);
-
-    if (bodyThisInContact === arena.body) {
-        //this.kill();
-        console.log("contactEnded!");
-    }
-};
 
 var updateArenaSize = function() {
-    //arena.body.onEndContact.removeAll();
     arena.scale.multiply(.999, .999);
     resizePolygon('arenaStrokeData', 'newArenaStrokeData', 'arenaStroke', arena.scale.x);
     arena.body.clearShapes();
     arena.body.loadPolygon('newArenaStrokeData', 'arenaStroke');
     arena.body.setCollisionGroup(arenaCollisionGroup);
-    //console.log(arena.body.addPolygon({}, 'newArenaStrokeData'));
-    //arena.body.setCircle(arena.width / 2);
-    //arena.body.static = true;
-    //arena.body.data.shapes[0].sensor = true;
-    //arena.body.onEndContact.add(contactEnded, arena);
-    //arena.body.setCollisionGroup(arenaCollisionGroup);
-    //arena.body.collides(playersCollisionGroup);
 };
 
-function resizePolygon(originalPhysicsKey, newPhysicsKey, shapeKey, scale){
+var resizePolygon = function(originalPhysicsKey, newPhysicsKey, shapeKey, scale) {
     var newData = [];
     var data = this.game.cache.getPhysicsData(originalPhysicsKey, shapeKey);
     for (var i = 0; i < data.length; i++) {
@@ -202,7 +165,6 @@ function resizePolygon(originalPhysicsKey, newPhysicsKey, shapeKey, scale){
     var item = {};
     item[shapeKey] = newData;
     game.load.physics(newPhysicsKey, '', item);
-    //debugPolygon(newPhysicsKey, shapeKey);
 };
 
 
@@ -216,7 +178,11 @@ var isPlayerWithinArena = function(player) {
     return false;
 };
 
-//function drawBackground() {
-    //graphics.beginFill(0x000000);
-    //graphics.drawRect(0, 0, game.width, game.height);
-//}
+var initiateTimer = function() {
+    //  Create our Timer
+    timer = game.time.create(false);
+    //  Set a TimerEvent to occur after 2 seconds
+    timer.loop(50, updateArenaSize, this);
+    //  Start the timer running
+    timer.start();
+};
