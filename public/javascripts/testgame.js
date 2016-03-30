de2691a09d51baef7ce55b7f7e244b854812f7ff
Adam Cole <adamcole@roberts-MacBook-Pro-2.local>
2016-03-30 18:34:43 -0400

    moved to gaga style game, puck, circle arena, correct collisions, and proper killing of character

--- public/javascripts/testgame.js
+++ public/javascripts/testgame.js
@@ -6,7 +6,9 @@ var graphics;
 var BACKGROUND_COLOR = '#ddd';
 var playersCollisionGroup;
 var arenaCollisionGroup;
+var puckCollisionGroup;
 var arena;
+var puck;
 var cursors;
 var wasd;
 var spacebar;
@@ -19,8 +21,10 @@ var player2;
 var moveVelocity = 100;
 
 function preload() {
-    game.load.image('player', 'images/player2.png');
-    game.load.image('arena', 'images/arena.png');
+    game.load.image('player', 'images/player.png');
+    game.load.image('arena', 'images/arenaStroke.png');
+    game.load.image('puck', 'images/puck.png')
+    game.load.physics('arenaStrokeData', 'images/objects/arenaStroke.json');
 
     game.stage.backgroundColor = BACKGROUND_COLOR;
 }
@@ -40,24 +44,40 @@ function create() {
 
     playersCollisionGroup = game.physics.p2.createCollisionGroup();
     arenaCollisionGroup = game.physics.p2.createCollisionGroup();
-
+    puckCollisionGroup = game.physics.p2.createCollisionGroup();
 
     var arenaradius = 580/2;
     arena = game.add.sprite(game.width / 2, game.height / 2, 'arena');
-    game.physics.p2.enable(arena, false);
+
+    game.physics.p2.enable(arena, true);
+    arena.body.clearShapes();
+    arena.body.loadPolygon('arenaStrokeData', 'arenaStroke');
+    arena.body.setCollisionGroup(arenaCollisionGroup);
+
+    var puckRadius = 5;
+    puck = game.add.sprite(game.width / 2, game.height / 2, 'puck');
+    puck.scale.setTo(.2,.2);
+    game.physics.p2.enable(puck, true);
+    puck.body.setCircle(puck.width / 2);
+
+    puck.body.setCollisionGroup(puckCollisionGroup);
+    puck.body.collides([playersCollisionGroup, arenaCollisionGroup]);
+
     //arena.body.setCircle(arenaradius);
-    //arena.body.static = true;
+    arena.body.static = true;
     //arena.body.data.shapes[0].sensor = true;
-    //arena.body.setCollisionGroup(arenaCollisionGroup);
-    //arena.body.collides(playersCollisionGroup);
+    arena.body.collides(puckCollisionGroup);
 
     players = game.add.group();
     players.enableBody = true;
     players.physicsBodyType = Phaser.Physics.P2JS;
     player1 = createPlayer();
-    player2 = createPlayer();
+    //player2 = createPlayer();
     //arena.body.onEndContact.add(contactEnded, arena);
     cursors = game.input.keyboard.createCursorKeys();
+    cursors.rotateRight = game.input.keyboard.addKey(Phaser.Keyboard.D);
+    cursors.rotateLeft = game.input.keyboard.addKey(Phaser.Keyboard.A);
+
     wasd = {
         up: game.input.keyboard.addKey(Phaser.Keyboard.W),
         down: game.input.keyboard.addKey(Phaser.Keyboard.S),
@@ -79,26 +99,26 @@ function create() {
 
 function update() {
     updatePlayer(player1, cursors);
-    updatePlayer(player2, wasd);
+    //updatePlayer(player2, wasd);
     if (!isPlayerWithinArena(player1)) player1.kill();
-    if (!isPlayerWithinArena(player2)) player2.kill();
+    //if (!isPlayerWithinArena(player2)) player2.kill();
 }
 
 var updatePlayer = function(player, controls) {
-    //if (controls.left.isDown) {
-    //    player.body.rotateLeft(100);
-    //    console.log("hello");
-    //} else if (controls.right.isDown) {
-    //    player.body.rotateRight(100);
-    //} else {
-    //    player.body.setZeroRotation();
-    //}
-    //
+    if (controls.rotateLeft.isDown) {
+        player.body.rotateLeft(200);
+    } else if (controls.rotateRight.isDown) {
+        player.body.rotateRight(200);
+    } else {
+        player.body.setZeroRotation();
+    }
+
     //if (controls.up.isDown) {
-    //    player.body.thrust(400);
+    //    player.body.thrust(150);
     //} else if (controls.down.isDown) {
-    //    player.body.reverse(400);
+    //    player.body.reverse(150);
     //}
+
     if (controls.left.isDown)
     {
         player.body.moveLeft(moveVelocity);
@@ -119,12 +139,19 @@ var updatePlayer = function(player, controls) {
 };
 
 var createPlayer = function() {
-    var player = players.create(game.width / 2, game.height / 2, 'player');
+    var player = players.create(game.width / 2, game.height / 2 + 200, 'player');
     player.scale.setTo(.20, .20);
     player.body.setCircle(player.width / 2);
+    player.body.addRectangle(125, 15);
     player.body.setCollisionGroup(playersCollisionGroup);
-
-    player.body.collides([playersCollisionGroup]);
+    player.body.collides([playersCollisionGroup, puckCollisionGroup]);
+    player.body.debug = true;
+    player.body.onBeginContact.add(function(bodyA, bodyB, shapeA, shapeB, contactEquations) {
+        if (shapeA.constructor.name === "Circle") {
+            player.body.debug = false;
+            player.kill();
+        }
+    });
     //player.body.onEndContact.add(contactEnded, player);
     player.body.mass = 2;
     player.body.damping = .8;
@@ -148,6 +175,11 @@ var contactEnded = function(bodyThisInContact, bodyThisEndedContact, shapeThatCa
 var updateArenaSize = function() {
     //arena.body.onEndContact.removeAll();
     arena.scale.multiply(.999, .999);
+    resizePolygon('arenaStrokeData', 'newArenaStrokeData', 'arenaStroke', arena.scale.x);
+    arena.body.clearShapes();
+    arena.body.loadPolygon('newArenaStrokeData', 'arenaStroke');
+    arena.body.setCollisionGroup(arenaCollisionGroup);
+    //console.log(arena.body.addPolygon({}, 'newArenaStrokeData'));
     //arena.body.setCircle(arena.width / 2);
     //arena.body.static = true;
     //arena.body.data.shapes[0].sensor = true;
@@ -155,6 +187,24 @@ var updateArenaSize = function() {
     //arena.body.setCollisionGroup(arenaCollisionGroup);
     //arena.body.collides(playersCollisionGroup);
 };
+
+function resizePolygon(originalPhysicsKey, newPhysicsKey, shapeKey, scale){
+    var newData = [];
+    var data = this.game.cache.getPhysicsData(originalPhysicsKey, shapeKey);
+    for (var i = 0; i < data.length; i++) {
+        var vertices = [];
+        for (var j = 0; j < data[i].shape.length; j += 2) {
+            vertices[j] = data[i].shape[j] * scale;
+            vertices[j+1] = data[i].shape[j+1] * scale;
+        }
+        newData.push({shape : vertices});
+    }
+    var item = {};
+    item[shapeKey] = newData;
+    game.load.physics(newPhysicsKey, '', item);
+    //debugPolygon(newPhysicsKey, shapeKey);
+};
+
 
 var isPlayerWithinArena = function(player) {
     var dx = arena.body.x - player.body.x; //distance ship X to enemy X
