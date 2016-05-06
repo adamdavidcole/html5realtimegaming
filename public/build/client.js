95b08d29d79857f3bd207103e6144c85816a7e30
Adam Cole <adamcole@roberts-MacBook-Pro-2.local>
2016-05-06 00:17:28 -0400

    updated io for heroku

--- public/build/client.js
+++ public/build/client.js
@@ -20,7 +20,7 @@ var pending_inputs = [];
 
 var room;
 
-var socket = io.connect("http://10.0.1.4:3000");
+var socket = io.connect("http://sheltered-tor-10865.herokuapp.com/");
 
 var init = function() {
     //renderer.init();
